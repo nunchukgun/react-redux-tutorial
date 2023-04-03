2d8c78e2212559f8aa5d21d2d720c8684cf24289
John McKeever <jmmckeever823@gmail.com>
2023-04-03 16:06:01 -0500

    added spaces to tests

--- src/app/exercises/redux/button-counter/BCSlice.test.js
+++ src/app/exercises/redux/button-counter/BCSlice.test.js
@@ -5,6 +5,7 @@ describe("BCSlice", () => {
     it("returns the initial state if provided an unprocessable action", () => {
       const action = { type: "bad_type", payload: "blah blah" };
       const result = bcReducer(undefined, action);
+      
       expect(result).toEqual(initialState);
     });
 
@@ -12,6 +13,7 @@ describe("BCSlice", () => {
       const action = { type: "bc/setCount", payload: 100 };
       const expected = { ...initialState, count: 100 };
       const result = bcReducer(initialState, action);
+
       expect(result).toEqual(expected);
     });
   });
